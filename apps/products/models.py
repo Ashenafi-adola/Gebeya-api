ff0a22f9f3a01182e70b139d6d9cf8ab858b9c77
ashenafi <ashenafiadola05@gmail.com>
2026-05-31 23:15:22 +0300

    defined the db models of the products app and added the app to the list of installed apps

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from accounts.models import Account
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=2)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product/images/')
+
+    def __str__(self):
+        return self.name
+
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    attribute = models.CharField(max_length=50)
+    value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.attribute} = {self.value}'
