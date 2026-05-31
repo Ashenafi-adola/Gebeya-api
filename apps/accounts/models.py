c60b5814eab5a3046a834f2f99f02354203422f3
ashenafi <ashenafiadola05@gmail.com>
2026-05-31 17:07:22 +0300

    defined custom user model in the accounts app

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Account(AbstractUser):
+    avater = models.ImageField(upload_to='avater/')
+    email = models.EmailField(unique=True, blank=False, null=False)
+
+    def __str__(self):
+        return self.username
+    
+    
