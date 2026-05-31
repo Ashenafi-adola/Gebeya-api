d16d690ea9bc059c31dc9b882ece7dc5b2397bec
ashenafi <ashenafiadola05@gmail.com>
2026-05-31 21:40:35 +0300

    added the address serializer

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from . models import Account
+from . models import Account, Address
 
 class AccountSerializer(ModelSerializer):
     class Meta:
         model = Account
         fields = ['username','first_name','last_name', 'avater', 'email', 'password']
         extra_kwargs = {'password':{'write_only': True}}
+
+class AddressSerializer(ModelSerializer):
+    class Meta:
+        model = Address
+        fields = "__all__"
+        extra_kwargs = {'user': {'read_only':True}}
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from . models import Account
-from . serializers import AccountSerializer
+from . serializers import AccountSerializer, 
 from rest_framework.permissions import AllowAny
 
 class CreateAccountAPIView(generics.CreateAPIView):
