2f9f767433fd6bd13881197afdd8bc51dfa46596
ashenafi <ashenafiadola05@gmail.com>
2026-05-31 21:45:00 +0300

    created the addaddressapiview

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
-from . models import Account
-from . serializers import AccountSerializer, 
-from rest_framework.permissions import AllowAny
+from . models import Account, Address
+from . serializers import AccountSerializer, AddressSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 class CreateAccountAPIView(generics.CreateAPIView):
     queryset = Account.objects.all()
@@ -11,6 +11,18 @@ class CreateAccountAPIView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         if serializer.is_valid():
+            serializer.save()
+        else:
+            pass
+
+class AddAddressAPIView(generics.CreateAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.user = self.request.user
             serializer.save()
         else:
             pass
