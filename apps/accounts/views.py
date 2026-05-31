35425c0cf507971a3755858e681114dac8443cce
ashenafi <ashenafiadola05@gmail.com>
2026-05-31 22:54:55 +0300

    added redirecting in the create method of the create account api view

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import generics
 from . models import Account, Address
 from . serializers import AccountSerializer, AddressSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import login, authenticate, logout
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
 
 class CreateAccountAPIView(generics.CreateAPIView):
     queryset = Account.objects.all()
@@ -11,9 +15,23 @@ class CreateAccountAPIView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         if serializer.is_valid():
-            serializer.save()
+            instance = serializer.save()
+            self.created_instance = instance
         else:
             pass
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        redirect_url = reverse(
+            'add-address'
+        )
+        return Response(
+            status=status.HTTP_302_FOUND,
+            headers={'Location': redirect_url}
+        )
 
 class AddAddressAPIView(generics.CreateAPIView):
     queryset = Address.objects.all()
