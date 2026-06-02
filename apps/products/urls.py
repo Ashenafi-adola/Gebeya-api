c60bcf29e3f290b35879ddc9793e6bfbaa0fc129
ashenafi <ashenafiadola05@gmail.com>
2026-06-02 10:29:31 +0300

    connected the url routing for the product app views

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . views import 
+from . views import AddCategoryAPIView, AddProductAPIView, AddProductAttributeAPIView
 
 urlpatterns = [
-    path('add-product/', )
+    path('add-category/', AddCategoryAPIView.as_view(), name='add-category'),
+    path('add-product/', AddProductAPIView.as_view(), name='add-product'),
+    path('add-product-attr/', AddProductAttributeAPIView.as_view(), name='add-pro-attr')
 ]
