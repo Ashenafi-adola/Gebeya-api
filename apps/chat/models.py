092fe2dcd99edde1602baefc9aad4fc8dd9b2f95
ashenafi <ashenafiadola05@gmail.com>
2026-06-15 22:43:35 +0300

    defined the message model and serilazer and message list view in the chat app

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.accounts.models import Account
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="senders")
+    reciever = models.ForeignKey(Account, on_delete=models.DO_NOTHING, related_name="recievers")
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from . models import Message
+from . serializers import MessageSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
+from apps.accounts.models import Account
 
-# Create your views here.
+
+class MessageListAPIView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = MessageSerializer
+    
+    def get_queryset(self):
+        return Message.objects.filter(
+            (
+                Q(sender=self.request.user) & Q(reciever=self.get_reciever())
+            ) | 
+            (
+                Q(reciever=self.request.user) & Q(sender=self.get_reciever())
+            )
+        )
+    
+    def get_reciever(self):
+        Account.objects.get(id=self.kwargs['pk'])
