6d66bbae09efd5889122c23a8352b2c6ab8d8295
Ashenafi <ashenafiadola05@gmil.com>
2026-07-13 20:32:16 +0300

    replaced related view with modelviewset in products app

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,17 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'myproducts', views.ManageMyProducts, basename='product')
+
 urlpatterns = [
     path('categories/', views.CategoriesAPIView.as_view()),
     path('products/', views.GetAllProductsAPIView.as_view()),
-    path('products/new/', views.AddProductAPIView.as_view()),
-    path('products-image/<int:pk>/', views.AddProductImage.as_view()),
-    path("product-detail/<int:pk>/", views.RetriveUpdateDestroyProductAPIView.as_view()),
-    path('categories/', views.CategoryListAPIView.as_view(), name='categories'),
-    path('retrive-cate/<int:pk>/', views.RetriveUpdateDestroyCategoryAPIView.as_view()),
-    path('cate-products/<int:pk>/', views.CategoryProductListAPIView.as_view(), name='cate-products'),
+    path('', include(router.urls)),
     path('pro-detail/<int:pk>/', views.ProductDetailAPIView.as_view(), name='pro-detail'),
     path('pro-fav/<int:pk>/', views.GetFavorities.as_view()),
-    path('my-pros/', views.GetMyProductsAPIView.as_view()),
     path('my-fav-products/', views.GetMyFavoriteProductsAPIView.as_view()),
     path('get-total-views/', views.GetMyTotalAPIView.as_view()),
 ]
