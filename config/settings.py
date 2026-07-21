f8714e145199afa29018a07f41fbebf08ea393f8
Ashenafi <ashenafiadola05@gmil.com>
2026-07-21 10:44:07 +0300

    fixed redis configuration issue

--- config/settings.py
+++ config/settings.py
@@ -137,10 +137,11 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT")
     }
 }
+
 CACHES = {
     "default": {
-        "BACKEND" : "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://gebeya_redis:6379/1",
     }
 }
 
