4d477658609bf7cce6a62208ca3f3ddec4bd20d7
ashenafi <ashenafiadola05@gmail.com>
2026-08-10 11:01:40 +0300

    removed redis config

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(BASE_DIR / "./env")
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = env.bool("DEBUG", default=False)
 
 if not DEBUG:
     ALLOWED_HOSTS = ["*"]
@@ -135,12 +135,12 @@ else:
         )
     }
 
-CACHES = {
+'''CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://gebeya_redis:6379/1",
     }
-}
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
