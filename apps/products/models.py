862aae1855bb661bfdd17b0235752d5a73b7b7fc
ashenafi <ashenafiadola05@gmail.com>
2026-08-13 16:27:30 +0300

    replaced product attribute model with attributes field in products model

--- apps/products/models.py
+++ apps/products/models.py
@@ -16,9 +16,13 @@ class Product(models.Model):
         ("approved", "Approved"),
         ("rejected", "Rejected"),
     )
-
+    FEATURED_STATUS = (
+        ("pending", "Pending"),
+        ("approved", "Approved"),
+        ("rejected", "Rejected"),
+    )
         
-    featured = models.CharField(default="NR", max_length=100)
+    featured = models.CharField(default="NR",choices=FEATURED_STATUS, max_length=100)
     name = models.CharField(max_length=50)
     price = models.DecimalField(decimal_places=2, max_digits=10)
     image = models.ImageField(upload_to="products/images", null=True, blank=True)
@@ -29,6 +33,7 @@ class Product(models.Model):
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     posted_date = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=50,choices=PRODUCT_STATUS ,default="Pending")
+    attributes = models.JSONField(null=True)
 
     class Meta:
         ordering = ["-posted_date"]
@@ -40,16 +45,6 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="products/images")
-
-
-class ProductAttribute(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    attribute = models.CharField(max_length=50)
-    value = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.attribute} = {self.value}"
-
 
 class Favorities(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
