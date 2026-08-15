3cbf5c4062b866618ebabcf1637601c18f940024
ashenafi <ashenafiadola05@gmail.com>
2026-08-15 08:49:04 +0300

    wrape channel layer hosts in a list

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": env("REDIS_URL"),
+            "hosts": [env("REDIS_URL")],
         },
     },
 }
